2a5093f4e0c7e1a4e9cc6f5ac3ef195e3a21f319
Aleksey <rusal@bk.ru>
2024-05-08 11:38:29 +0300

    fix

--- cpp_СoursePaper2024.cpp
+++ cpp_СoursePaper2024.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
 
 struct ExamsRecords {
-	char name[20];
+	char name[20]; // Название дисциплины
 	int mark;
 	// 0 - не зачтено
 	// 1 - зачтено  
@@ -11,25 +14,36 @@ struct ExamsRecords {
 
 struct StudentNode
 {
-	char surName[20];
-	char name[20];
-	char middleName[20];
-	char faculty[20];
-	char department[20];
-	char group[20];
-	char recordCardNumber[20];
-	char birthDateString[20];
-	bool sex[20]; // true - мальчик
+	char surName[20]; // Фамилия
+	char name[20]; // Имя
+	char middleName[20]; // Отчество
+	char faculty[20]; // Факультет (название института)
+	char department[20]; // Название кафедры
+	char group[20]; // шифр группы кафедры
+	char recordCardNumber[20]; // зачетной книжки
+	// int recordCardNumber; // для некоторых вариантов заданий допустимо использовать 
+	// целочисленное значение зачетной книжки
+	char birthDateString[20]; // дата рождения 
+	// int birthDay;
+	// int birth;
+	// int birthDay;
+	bool sex[20]; // Флак пола true - мальчик
 	// false - девочка
-	int startYear;
-
-	ExamsRecords examsRecordsData[9][10];
-	StudentNode* next;
-	int id;
+	// Предполагается бинарный пол
+	int startYear; // Год начала обучения
+	ExamsRecords examsRecordsData[11][10]; // данные о сессиях 5,5 лет для БИСО 
+	// для БИСО 11 семестров, для БББО 8 семестров - 4 года
+	StudentNode* next; // указатель на следующий элемент для динамического списка
+	// int id; // id вспомогательная переменная (удобно для некоторых заданий) 
+	// float avrMark; // средний балл по оценкам (удобно для некоторых заданий)
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+	SetConsoleOutputCP(1251);
+	StudentNode st1;
+    cout << "Размер записи студента\n" << sizeof(st1) << endl << sizeof(StudentNode);
 }
 
