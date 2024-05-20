dea8d542a0c1bf625d059784e971ab2e2abe8c23
Rusakov Aleksey <rusal@bk.ru>
2024-05-21 00:56:05 +0300

    fix

--- 05 Class Student/05 Class Student.cpp
+++ 05 Class Student/05 Class Student.cpp
@@ -9,7 +9,7 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
 	SetConsoleOutputCP(1251);
-	cout << "04 Add student\n";
+	cout << "05 Class Student\n";
 	
 	StudentClass* st = new StudentClass();
 	StudentNode* sn = new StudentNode();
