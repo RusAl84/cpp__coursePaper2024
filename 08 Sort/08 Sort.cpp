453d04be1031c5c119c761c002b416810cd0b3f6
Rusakov Aleksey <rusal@bk.ru>
2024-05-22 12:50:27 +0300

    class diagram

--- 08 Sort/08 Sort.cpp
+++ 08 Sort/08 Sort.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <string>
 #include "../05 Class Student/StudentClass.h"
-
+//https://learn.microsoft.com/ru-ru/visualstudio/ide/class-designer/designing-and-viewing-classes-and-types?view=vs-2022
 
 using namespace std;
 
